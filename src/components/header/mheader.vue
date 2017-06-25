6003fa62635e0d98ae7188fef324aaf2e01368a9
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-25 23:28:19 +0800

    added mheader component

---
+++ src/components/header/mheader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <h1 class="title">Tune</h1>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/variable.styl"
+  @import "../../common/stylus/mixin.styl"
+
+  .header
+    position: relative
+    height: 44px
+    text-align: center
+    color: $color-theme
+    .title
+      display: inline-block
+      line-height: 44px
+      font-size: $font-size-large
+
+</style>
