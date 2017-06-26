e82442c7fe29542a51106ddc491f9dc715476351
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-26 13:51:24 +0800

    added jsonp utility functions to grab data from qq music

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,26 @@
+/**
+ * Created by mac on 26/06/2017.
+ */
+import origJSONP from 'jsonp';
+
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data);
+  return new Promise((resolve, reject) => {
+    origJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data);
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+function param (data) {
+  let url = '';
+  for (let k in data) {
+    let value = data[k] !== undefined ? data[k] : '';
+    url += `&${k}=${encodeURIComponent(value)}`;
+  }
+  return url ? url.substring(1) : '';
+}
