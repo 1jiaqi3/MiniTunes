2354b95ef27fa7284b9206bd358ca8bf3825177d
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-27 17:33:47 +0800

    got performers data in jsonp from qq music

---
+++ src/common/api/performer.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mac on 27/06/2017.
+ */
+import jsonp from '../../common/js/jsonp';
+import {commonParams, options} from './config';
+
+export function getPer() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg';
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 150,
+    pagenum: 1,
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    g_tk: 1664029744
+  });
+  return jsonp(url, data, options);
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,36 @@
 <template>
-
+  <div class="singer"></div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  import {getPer} from '../../common/api/performer';
+  import {ERR_OK} from '../../common/api/config';
+  export default {
+    data() {
+      return {
+        singers: []
+      };
+    },
+    created() {
+      this._getSingerList();
+    },
+    methods: {
+      _getSingerList() {
+        getPer().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list;
+            console.log(this.singers);
+          }
+        });
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
