510f00ac2aec0e571e81df83c49a95403136e71f
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-06-29 21:47:07 +0800

    added essential styles to rank component

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="rank">
-    <div class="topList">
+    <scroll class="toplist" :data="topList">
       <ul>
-        <li class="item">
+        <li class="item" v-for="item in topList">
           <div class="icon">
-            <img/>
+            <img v-lazy="item.picUrl" width="100" height="100"/>
           </div>
-          <ul class="songList">
-            <li class="song">
-              <span></span>
-              <span></span>
+          <ul class="songlist">
+            <li class="song" v-for="(song, idx) in item.songList">
+              <span>{{idx + 1}}</span>
+              <span>{{song.songname}}-{{song.singername}}</span>
             </li>
           </ul>
         </li>
       </ul>
-    </div>
+      <div class="loading" v-show="!topList.length">
+        <loading></loading>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import {getTopList} from '../../common/api/rank';
   import {ERR_OK} from '../../common/api/config';
+  import Scroll from '../../base/scroll/scroll';
+  import Loading from '../../base/loading/loading';
   export default {
+    data() {
+      return {
+        topList: []
+      };
+    },
     created() {
       this._getTopList();
     },
@@ -29,10 +39,14 @@
       _getTopList() {
         getTopList().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.topList);
+            this.topList = res.data.topList;
           }
         });
       }
+    },
+    components: {
+      Scroll,
+      Loading
     }
   };
 </script>
@@ -40,5 +54,45 @@
 <style lang="stylus" rel="stylesheet/stylus">
   @import '../../common/stylus/variable.styl'
   @import '../../common/stylus/mixin.styl'
+
+  .rank
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .toplist
+      height: 100%
+      overflow: hidden
+      .item
+        display: flex
+        margin: 0 20px
+        padding-top: 20px
+        height: 100px
+        &.last-child
+          padding-bottom: 20px
+        .icon
+          flex: 0 0 100px
+          width: 100px
+          height: 100px
+        .songlist
+          flex: 1
+          display: flex
+          flex-direction: column
+          justify-content: center
+          padding: 0 20px
+          height: 100px
+          overflow: hidden
+          background: $color-highlight-background
+          color: $color-text-d
+          font-size: $font-size-small
+          .song
+            no-wrap()
+            line-height: 26px
+      .loading
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
+
 
 </style>
